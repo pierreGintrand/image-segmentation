9009f1dce54f919bfd071a359b31fc0e7ea291ec
PierreGintrand <pierre.gintrand@supelec.fr>
2017-06-13 01:37:13 +0900

    show the picture

--- main.py
+++ main.py
@@ -12,14 +12,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 filename = 'pictures/pills.jpg'
-filename = 'pictures/rouge.png'
+#filename = 'pictures/couleur.png'
 
-#Load the image in RGB space
+#constants
+hr = 19
+hs = 16
+M = 40
+
+#Load the image in BGR space
 img = cv2.imread(filename, cv2.IMREAD_COLOR)
-img = img.astype(np.int32)
 
 #convert to L*a*b* space
-img = cv2.cvtColor(img, cv2.COLOR_RGB2Lab)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
 
 
 
+img = cv2.cvtColor(img, cv2.COLOR_Lab2RGB)
+
+
+
+
+plt.figure()
+plt.imshow(img)
+plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
+plt.show()
